3d9b93e9a828ad036d1eea71341cf93a3ad11d92
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2025-12-31 12:52:24 +0330

    added error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,9 +1,11 @@
 // Codes by mahdi tasha
+// Forcing next.js to render used components of this hook render by client side
+"use client";
+
 // Importing part
 import Footer from "@/component/footer";
 import Header from "@/component/header";
 import { Button } from "@/component/ui/button";
-import Link from "next/link";
 import { JSX } from "react";
 
 // Creating and exporting 404 page as default
@@ -15,15 +17,14 @@ export default function NotFoundPage(): JSX.Element {
       <section className="w-dvw">
         <main className="mx-auto max-w-4xl p-4 flex items-center justify-center min-h-dvh">
           <div>
-            <h1 className="mt-0 mb-3 text-center">404 - Page Not Found</h1>
+            <h1 className="mt-0 mb-3 text-center">Something went wrong</h1>
             <p className="text-center mb-5">
-              Sorry, the page you’re looking for doesn’t exist or has bee moved.
-              <br />
-              Let’s get you back on track.
+              An unexpected error occurred while processing your request.
+              <br /> Please try again, or come back later.
             </p>
             <div className="flex items-center justify-center">
-              <Button size="lg" asChild className="no-underline">
-                <Link href="/">Go back to Home</Link>
+              <Button size="lg" onClick={() => window.location.reload()}>
+                Reload the page
               </Button>
             </div>
           </div>
